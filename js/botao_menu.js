5b2c2519315afe54c80ff4ed430c400f0149307a
Pedro Arthur Melo Rodrigues <pedro.arthur1@pucpr.edu.br>
2025-05-13 19:05:29 -0300

    botão menu nas páginas

---
+++ js/botao_menu.js
@@ -0,0 +1,12 @@
+function toggleDropdown() {
+    const dropdown = document.querySelector('.dropdown-content');
+    dropdown.classList.toggle('active');
+}
+
+// Fecha o menu se clicar fora dele
+window.addEventListener('click', function(event) {
+    const dropdown = document.querySelector('.dropdown-content');
+    if (!dropdown.contains(event.target) && !event.target.matches('.nav-user')) {
+        dropdown.classList.remove('active');
+    }
+});
